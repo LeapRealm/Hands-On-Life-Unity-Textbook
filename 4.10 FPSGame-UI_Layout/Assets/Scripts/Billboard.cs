65bbbfbd65144e016cf94e1275c6938fc57e3395
Jinwoo, Kim <admin@leaprealm.com>
2020-07-25 01:49:26 +0900

    FPS Game - UI Design Layout

---
+++ 4.10 FPSGame-UI_Layout/Assets/Scripts/Billboard.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Billboard : MonoBehaviour
+{
+    public Transform cameraTransform;
+    
+    private void Update()
+    {
+        // 자기 자신의 방향을 카메라의 방향과 일치시킨다.
+        transform.forward = cameraTransform.forward;
+    }
+}
